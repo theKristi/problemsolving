58e85a8a8e4edf0745a076eec80e04c24ffd1aea
Kristi <kristithecoder@gmail.com>
2018-12-19 06:44:04 -0800

    Updates

---
+++ precache-manifest.18d16a6b33f8b41b944a5512a048860a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d0e6e8e1861d4f06c890",
+    "url": "/static/js/main.d0e6e8e1.chunk.js"
+  },
+  {
+    "revision": "9af8126648b0cf30515a",
+    "url": "/static/js/1.9af81266.chunk.js"
+  },
+  {
+    "revision": "d0e6e8e1861d4f06c890",
+    "url": "/static/css/main.38d3d2ed.chunk.css"
+  },
+  {
+    "revision": "9af8126648b0cf30515a",
+    "url": "/static/css/1.2f924b7b.chunk.css"
+  },
+  {
+    "revision": "5f7cde871d2d09f3a4b4f2ec6e14c443",
+    "url": "/index.html"
+  }
+];
