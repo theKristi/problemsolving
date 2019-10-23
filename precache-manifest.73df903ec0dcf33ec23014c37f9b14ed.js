c2736346725204794d35aee223d417b2e0e0f6d6
Kristi Marks <ZG5804@engie.com>
2019-10-23 10:17:27 -0700

    Updates

---
+++ precache-manifest.73df903ec0dcf33ec23014c37f9b14ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c8bb9c6e77f546a9ca24805fb164003",
+    "url": "/problemsolving/index.html"
+  },
+  {
+    "revision": "841744cbdad600368d3a",
+    "url": "/problemsolving/static/css/2.619dee99.chunk.css"
+  },
+  {
+    "revision": "51a3d157c0bfd87c8984",
+    "url": "/problemsolving/static/css/main.3deb250d.chunk.css"
+  },
+  {
+    "revision": "841744cbdad600368d3a",
+    "url": "/problemsolving/static/js/2.6b8d54cd.chunk.js"
+  },
+  {
+    "revision": "51a3d157c0bfd87c8984",
+    "url": "/problemsolving/static/js/main.c60bc860.chunk.js"
+  },
+  {
+    "revision": "5ddf7ac50eb08a16f528",
+    "url": "/problemsolving/static/js/runtime-main.781cb97c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/problemsolving/precache-manifest.30cd9666f6453828c86f3fca81df13f5.js"
+  "/problemsolving/precache-manifest.73df903ec0dcf33ec23014c37f9b14ed.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/problemsolving/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/problemsolving/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
